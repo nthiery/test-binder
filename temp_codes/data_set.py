c7ab4348958c4675c7598c5d7ae01550e9752aa0
Nicolas M. Thiéry <nthiery@users.sf.net>
2016-12-03 15:19:56 +0100

    Added missing files (data_set class and upload)

---
+++ temp_codes/data_set.py
@@ -0,0 +1,48 @@
+import pandas
+import numpy
+from io import StringIO
+from temp_codes.ls_5 import line_source
+
+class DataSet:
+    def __init__(self, file=None, csv=None):
+        if csv:
+            assert file is None
+            file = StringIO(csv)
+        self.data = pandas.read_csv(file, sep=",", skiprows=[0])
+
+    def __repr__(self):
+        return "A XXX model with %s measures"%(len(self.temperature_responses))
+
+    @property
+    def temperatures(self):
+        return self.data.set_index("t_s")['T']
+
+    @property
+    def times(self):
+        #return self.data.iloc[10:]['t_s']
+        return self.data.iloc[11:]['t_s']
+
+    @property
+    def temperature_responses(self):
+        """
+            >>> import sys
+            >>> sys.path.append(".")
+            >>> from temp_codes.data_set import DataSet
+            >>> data_set = DataSet("temp_codes/agar_data_try.dat")
+            >>> data_set.temperature_responses
+            t_s
+            0.0      0.002082
+            0.5      0.003143
+            ...
+            179.5    0.481493
+            180.0    0.489195
+            Name: T, dtype: float64
+        """
+        t = self.temperatures
+        ti = t.iloc[:10]
+        # TODO: determine the first i such that t > 0
+        #return t.iloc[10:] - ti.mean()
+        return t.iloc[11:] - ti.mean()
+
+    def line_source(self, **kwargs):
+        return line_source(tim=numpy.asarray(self.temperature_responses.keys()), **kwargs)
---
+++ temp_codes/data_set_widget.py
@@ -0,0 +1,57 @@
+from __future__ import print_function # py 2.7 compat.
+import ipywidgets as widgets # Widget definitions.
+from traitlets import Unicode # Traitlet needed to add synced attributes to the widget.
+
+class FileWidget(widgets.DOMWidget):
+    _view_name = Unicode('FilePickerView').tag(sync=True)
+    _view_module = Unicode('filepicker').tag(sync=True)
+
+    value = Unicode().tag(sync=True)
+    filename = Unicode().tag(sync=True)
+
+    def __init__(self, **kwargs):
+        """Constructor"""
+        widgets.DOMWidget.__init__(self, **kwargs) # Call the base.
+
+        # Allow the user to register error callbacks with the following signatures:
+        #    callback()
+        #    callback(sender)
+        self.errors = widgets.CallbackDispatcher(accepted_nargs=[0, 1])
+
+        # Listen for custom msgs
+        self.on_msg(self._handle_custom_msg)
+
+    def _handle_custom_msg(self, content):
+        """Handle a msg from the front-end.
+
+        Parameters
+        ----------
+        content: dict
+            Content of the msg."""
+        if 'event' in content and content['event'] == 'error':
+            self.errors()
+            self.errors(self)
+
+class DataSetUploadWidget(FileWidget):
+    def __init__(self, data_set_cls, **kwargs):
+        self._data_set_cls = data_set_cls
+        FileWidget.__init__(self, **kwargs)
+        self.observe(self.file_loading, names='filename')
+        self.observe(self.file_loaded,  names='value')
+        self.errors.register_callback(self.file_failed)
+
+    # Register an event to echo the filename when it has been changed.
+    def file_loading(self, change):
+        print("Loading %s" % self.filename)
+
+    # Register an event to parse the file content after the upload
+    def file_loaded(self, change):
+        self.data_set = self._data_set_cls(csv=self.value)
+
+    # Register an event to print an error message when a file could not
+    # be opened.  Since the error messages are not handled through
+    # traitlets but instead handled through custom msgs, the registration
+    # of the handler is different than the two examples above.  Instead
+    # the API provided by the CallbackDispatcher must be used.
+    def file_failed(self):
+        print("Could not load file contents of %s" % self.filename)
